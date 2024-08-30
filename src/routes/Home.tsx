78a1993aa546aa23f94185ea3ec0457acdbc7946
Gustavo Mazurkiewitz <gmazurkiewitz@gmail.com>
2024-08-30 11:24:37 -0300

    Exibindo os dados do usuário e estilização do card de usuário

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,18 +1,32 @@
-import { UserProps } from "../types/user";
+import { UserProps } from "../types/user"
 
 import Search from "../components/Search"
 import Loading from "../components/Loading"
+import User from "../components/User"
 
 import { useState } from "react"
+import Error from "../components/Error"
 
 function Home() {
   const [user, setUser] = useState<UserProps | null>(null);
+  const [error, setError] = useState<boolean>(false);
   const [showLoading, setShowLoading] = useState<boolean>(false);
+
   const loadUser = async (userName: string) => {
     setShowLoading(true)
+    setError(false)
+    setUser(null)
+
     const res = await fetch(`https://api.github.com/users/${userName}`)
     const data = await res.json()
     
+    setShowLoading(false)
+    
+    if (res.status === 404) {
+      setError(true)
+      return
+    }
+
     const {avatar_url, login, location, followers, following} = data
     const userData: UserProps = {
       avatar_url,
@@ -21,7 +35,6 @@ function Home() {
       followers,
       following
     }
-    setShowLoading(false)
     setUser(userData)
   }
 
@@ -29,7 +42,10 @@ function Home() {
     <div>
       <Search loadUser={loadUser} />
       {user && !showLoading &&
-        <p>{user.login}</p>
+        <User {...user}/>
+      }
+      {error &&
+        <Error />
       }
       {showLoading &&
         <Loading />
